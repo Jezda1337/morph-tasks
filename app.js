39194503963a5a8cca6fbab046d732a0cae2bba8
Jezda1337 <megasrbin1@gmail.com>
2021-06-15 13:36:45 +0200

    :rocket:

--- app.js
+++ app.js
@@ -1,40 +1,65 @@
 const progressBorder = document.querySelector("#progressBorder");
 const steps = document.querySelectorAll("#steps");
 const progress = document.querySelector("#progress");
+const inputs = document.querySelectorAll("input");
 
-(() => {
-  const firstName = document.querySelector("#firstName");
-  firstName.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    firstName.value
-      ? ((steps[0].innerHTML = "&#10004;"), (progress.innerHTML = `25%`))
-      : (steps[0].innerHTML = "1");
-    progressBorder.style.borderTop = "2px solid red";
+const firstName = document.querySelector("#firstName");
+firstName.addEventListener("change", () => {
+  if (firstName.value != "") {
+    progressBorder.style.borderTop = "2px solid white";
     progressBorder.style.transition = "border .5s ease";
-  });
-})();
+    steps[0].innerHTML = "&#10004";
+    progress.textContent = parseInt(progress.textContent) + 25 + "%";
+  } else {
+    steps[0].innerHTML = "1";
+    progressBorder.style.borderTop = "2px solid white";
+    progress.textContent = parseInt(progress.textContent) - 25 + "%";
+  }
+});
 
-(() => {
-  const lastName = document.querySelector("#lastName");
-  lastName.addEventListener("change", (e) => {
-    console.log(e.target.value);
-
-    lastName.value
-      ? ((steps[1].innerHTML = "&#10004;"), (progress.innerHTML = `50%`))
-      : (steps[1].innerHTML = "2");
-    progressBorder.style.borderRight = "2px solid red";
+const lastName = document.querySelector("#lastName");
+lastName.addEventListener("change", () => {
+  if (lastName.value != "") {
+    progressBorder.style.borderRight = "2px solid white";
     progressBorder.style.transition = "border .5s ease";
-  });
-})();
+    steps[1].innerHTML = "&#10004";
+    progress.textContent = parseInt(progress.textContent) + 25 + "%";
+  } else {
+    steps[1].innerHTML = "2";
+    progressBorder.style.borderRight = "2px solid white";
+    progress.textContent = parseInt(progress.textContent) - 25 + "%";
+  }
+});
 
-(() => {
-  const email = document.querySelector("#email");
-  email.addEventListener("change", (e) => {
-    console.log(e.target.value);
-    progressBorder.style.borderBottom = "2px solid red";
+const email = document.querySelector("#email");
+email.addEventListener("change", () => {
+  if (email.value != "") {
+    progressBorder.style.borderBottom = "2px solid white";
     progressBorder.style.transition = "border .5s ease";
-    email.value
-      ? ((steps[2].innerHTML = "&#10004;"), (progress.innerHTML = `75%`))
-      : (steps[2].innerHTML = "3");
+    steps[2].innerHTML = "&#10004";
+    progress.textContent = parseInt(progress.textContent) + 25 + "%";
+  } else {
+    steps[2].innerHTML = "3";
+    progressBorder.style.borderBottom = "2px solid white";
+    progress.textContent = parseInt(progress.textContent) - 25 + "%";
+  }
+});
+
+const checkBox = document.getElementsByName("contact");
+checkBox.forEach((box) => {
+  box.addEventListener("change", () => {
+    if (box.checked) {
+      steps[3].innerHTML = "&#10004";
+      progressBorder.style.borderLeft = "2px solid white";
+      progressBorder.style.transition = "border .5s ease";
+      progress.textContent = parseInt(progress.textContent) + 25 + "%";
+    } else {
+      progress.textContent = parseInt(progress.textContent) - 25 + "%";
+    }
+    if (progress.textContent == "100%") {
+      progress.innerHTML = "&#10004";
+    } else if (progress.textContent > "100%") {
+      progress.innerHTML = "&#10004";
+    }
   });
-})();
+});
