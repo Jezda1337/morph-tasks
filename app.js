2c5e4e3d9d293d89ecdca616fdb3cd7cba2f1c4c
Jezda1337 <megasrbin1@gmail.com>
2021-06-15 14:51:10 +0200

    :rocket:

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("test");
+const lowerCaseLetters = /[a-z]/g;
+const upperCaseLetters = /[A-Z]/g;
+const numbers = /[0-9]/g;
+
+const validatePassword = (password) => {
+  if (
+    password.match(lowerCaseLetters) &&
+    password.match(upperCaseLetters) &&
+    password.match(numbers) &&
+    password.length >= 8
+  ) {
+    console.log(`Your password is strong. :)`);
+  } else if (
+    password.match(lowerCaseLetters) &&
+    password.match(upperCaseLetters) &&
+    password.length >= 6
+  ) {
+    console.log(`Your password is a medium-strength. :)`);
+  } else if (password.length >= 3) {
+    console.log(
+      `Your password is weak strength! Please try another one but stronger. :)`
+    );
+  }
+};
+
+//enter your password here :)
+validatePassword("aleksandar1");
